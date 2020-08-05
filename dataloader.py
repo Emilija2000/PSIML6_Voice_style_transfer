3753895ba9957e103ca7315de55d5734943d8767
Emilija2000 <emilidjordjevic@gmail.com>
2020-08-05 19:33:54 +0200

    loads dataset

--- dataloader.py
+++ dataloader.py
@@ -6,20 +6,51 @@ from collections import defaultdict
 import numpy as np
 import torch
 import torchaudio
+import torchvision
 import matplotlib.pyplot as plt
+import csv
+import os
 
-AudioSegment.converter = "C:\\ffmpeg-20200802-b48397e-win64-static\\ffmpeg-20200802-b48397e-win64-static\\bin\\ffmpeg"
-
+AAudioSegment.converter = "ffmpeg"
 torchaudio.set_audio_backend = "SoundFile"
 
-if __name__=='__main__':
-    # files                                                                         
-    src = "D:\\Accents\\4114_6391_bundle_archive\\recordings\\recordings\\albanian5.mp3"
-    dst = "test.wav"
+def readAudio(mp3_path):
+    # files             
+    dst = mp3_path[:-4] + ".wav"
 
-# convert wav to mp3                                                            
-    sound = AudioSegment.from_mp3(src)
-    sound.export(dst, format="wav")
+    # convert mp3 to wav   
+    if not os.path.exists(dst):                                                         
+        sound = AudioSegment.from_mp3(mp3_path)
+        sound.export(dst, format="wav")
+        
     test,sr = torchaudio.load_wav(dst)
-    print(test)
     
+    return test, sr
+    
+class AccentDataset(torch.utils.data.Dataset):
+    def __init__(self, path, csv_path):
+        self.data_names = []
+        self.classes = []
+        self.encod_data = []
+
+        with open(csv_path) as csvfile:
+            csv_reader = csv.reader(csvfile,delimiter=',')
+            for row in csv_reader:
+                if (row[8] == "FALSE"):
+                    mp3_path = os.path.join(path,row[3]+'.mp3')
+                    self.data_names.append((mp3_path, row[4]))
+                    if (row[4] not in self.classes):
+                        self.classes.append(row[4])
+                    
+            for data in self.data_names:
+                label = torch.zeros(len(self.classes)) 
+                label[self.classes.index(data[1])] = 1
+                self.encod_data.append((data[0],label))
+
+    def __getitem__(self,index):
+        dat, s_rate = readAudio(self.data_names[index][0])
+        label = self.encod_data[index][1]
+        return dat, label
+
+    def __len__(self):
+        return len(all_data)
